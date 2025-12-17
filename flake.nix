95fa5c782d9ead951fe72e18f81fd3d9c564f86f
kaizen4dev <186304347+kaizen4dev@users.noreply.github.com>
2025-12-17 19:06:46 +0200

    Add flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      forAllSystems = function:
+        nixpkgs.lib.genAttrs [
+          "x86_64-linux"
+          "x86_64-darwin"
+          "aarch64-linux"
+          "aarch64-darwin"
+        ] (system: function nixpkgs.legacyPackages.${system});
+    in {
+      packages = forAllSystems (pkgs: {
+        default = pkgs.callPackage ./default.nix {};
+      });
+    };
+}
+
